b1c59f251b02df04e36bf5f26100923fbe5e9c11
nyera <nyeragbr@hotmail.com>
2023-09-18 13:20:14 -0700

    Task 1

--- main.h
+++ main.h
@@ -6,8 +6,7 @@
 
 int print_char(char c);
 int print_string(char *str);
+int print_integer(int num);
 int _printf(const char *format, ...);
 int process_format(const char **format, va_list args);
-int _putchar(char c);
-int print_integer(int num);
 #endif /* _MAIN_H_ */
