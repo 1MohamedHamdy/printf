2358938ef531220162474442597cb2d401b69a2c
nyera <nyeragbr@hotmail.com>
2023-09-18 13:01:01 -0700

    update Task1

--- helper_fuctions.c
+++ helper_fuctions.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <unistd.h>
-
+#include <stdio.h>
 /**
  * print_char - Helper function to print a character.
  * @c: The character to print.
@@ -30,4 +30,25 @@ int print_string(char *str)
 		str++;
 	}
 	return (count);
+}
+/**
+ * *print_interger - Helper function to ptint an integer.
+ * @num : the nteger to print
+ *
+ * Return : the number of characters printed.
+ */
+int print_integer(int num)
+{
+	int num_len = num < 0?1 : 0;
+	int temp = num;
+
+	while (temp != 0)
+	{
+		temp /=10;
+		num_len++;
+	}
+	char buffer[20];
+	snprintf(buffer, sizeof(buffer), "%d",num);
+
+	return (print_string(buffer));
 }
