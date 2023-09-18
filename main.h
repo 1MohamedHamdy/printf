9ab69008cb22648eeacd065cdd0f4a0cf2994900
nyera <nyeragbr@hotmail.com>
2023-09-18 07:47:07 -0700

    task 1

--- main.h
+++ main.h
@@ -2,10 +2,12 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
+#include <stdio.h>
 
 int print_char(char c);
 int print_string(char *str);
 int _printf(const char *format, ...);
 int process_format(const char **format, va_list args);
+int _putchar(char c);
 
 #endif /* _MAIN_H_ */
