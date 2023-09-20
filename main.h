20f16e06634a131cc46973ec6da996bb216f7427
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-20 13:50:29 -0700

    Updated main.h

--- main.h
+++ main.h
@@ -5,32 +5,26 @@
 
 int print_char(char c);
 int print_string(char *str);
+int print_integer(int num);
+int print_binary(unsigned int num);
+int print_unsigned_int(unsigned int num);
+int print_octal(unsigned int num);
+int print_hex_lower(unsigned int num);
+int print_hex_upper(unsigned int num);
+int process_return(const char ***format,int *count, va_list args);
+int process_percent(void);
+int process_default(const char ***format);
+int process_char(va_list args);
+int process_string(va_list args);
+int process_integer(va_list args);
+int process_binary(va_list args);
+int process_unsigned_int(va_list args);
+int process_octal(va_list args);
+int process_hex_lower(va_list args);
+int process_hex_upper(va_list args);
 int _printf(const char *format, ...);
 int process_format(const char **format, va_list args);
-int print_integer (int num);
-int print_binary(unsigned int num);
-
-
-/**
-* Task 3 
-*/
-/**
- * struct specifier - Struct token
- *
- * @specifier: format token
- * @f: The function associated
- */
-typedef struct specifier
-{
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} specifier_t;
-
-int print_unsigned(va_list args, params_t *params);
-int print_hex(va_list args, params_t *params);
-int print_HEX(va_list args, params_t *params);
-int print_octal(va_list args, params_t *params);
-void init_params(params_t *params, va_list args);
 
 
 #endif /* _MAIN_H_ */
+
