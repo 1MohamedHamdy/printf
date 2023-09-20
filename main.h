afc4999b7331bbda8ca13064364f1c3a9268d299
NyeraGbr <34568106+nyera@users.noreply.github.com>
2023-09-20 17:21:26 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,15 +5,10 @@
 
 int print_char(char c);
 int print_string(char *str);
-<<<<<<< HEAD
 int _printf(const char *format, ...);
 int process_format(const char **format, va_list args);
 int print_integer (int num);
-=======
-int print_integer(int num);
 int print_binary(unsigned int num);
-int _printf(const char *format, ...);
-int process_format(const char **format, va_list args);
+int print_unsigned(va_list ap, params_t *params);
 
->>>>>>> 01e354f6a673fddedd3787271b1cb9208728280f
 #endif /* _MAIN_H_ */
